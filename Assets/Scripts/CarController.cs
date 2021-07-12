9fbdd001fbe65fa88748ccf18818a1661b2de558
devwithu <devwithu@naver.com>
2021-07-12 09:35:15 +0900

    init

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public GameObject[] bodyObjects;
+    public Color32[] colors;
+
+    Material[] carMats;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // carMats 배열을 초기화한다.
+        carMats = new Material[bodyObjects.Length];
+
+        // 색상 담당 오브젝트에서 매터리얼을 carMats 배열에 등록한다.
+        for (int i = 0; i < carMats.Length; i++)
+        {
+            carMats[i] = bodyObjects[i].GetComponent<MeshRenderer>().material;
+        }
+
+        // 색상 배열 0번에 자동차의 기본 색상을 저장한다.
+        colors[0] = carMats[0].color;
+    }
+
+    public void ChangeColor(int num)
+    {
+        for(int i = 0; i<carMats.Length; i++)
+        {
+            carMats[i].color = colors[num];
+            Debug.LogError("=========================================");
+        }
+    }
+}
---
+++ Assets/Scripts/CarManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using UnityEngine.EventSystems;
+
+public class CarManager : MonoBehaviour
+{
+    public GameObject indicator;
+    public GameObject myCar;
+
+    ARRaycastManager arManager;
+    GameObject placeObject;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        indicator.SetActive(false);
+
+        arManager = GetComponent<ARRaycastManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DetectGround();
+
+        if (EventSystem.current.currentSelectedGameObject)
+        {
+            return;
+        }
+
+        if(indicator.activeInHierarchy && Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if(touch.phase == TouchPhase.Began)
+            {
+                if(placeObject == null)
+                {
+                    placeObject = Instantiate(myCar, indicator.transform.position, indicator.transform.rotation);
+                }
+                else
+                {
+                    placeObject.transform.SetPositionAndRotation(indicator.transform.position, indicator.transform.rotation);
+
+                }
+                
+            }
+        }
+    }
+
+    void DetectGround()
+    {
+        Vector2 screenSize = new Vector2(Screen.width * 0.5f, Screen.height * 0.5f);
+
+        List<ARRaycastHit> hitInfos = new List<ARRaycastHit>();
+
+        if(arManager.Raycast(screenSize, hitInfos, TrackableType.Planes))
+        {
+            indicator.SetActive(true);
+
+            indicator.transform.position = hitInfos[0].pose.position;
+            indicator.transform.rotation = hitInfos[0].pose.rotation;
+
+            indicator.transform.position += indicator.transform.up * 0.1f;
+        }
+        else
+        {
+            indicator.SetActive(false);
+        }
+    }
+}
